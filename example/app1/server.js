9466a567ee73692fadb8a94f36adee2641e73148
plasmashadow <plasmashadowx@gmail.com>
2016-02-08 08:15:03 +0530

    more and more changes

---
+++ example/app1/server.js
@@ -0,0 +1,14 @@
+var http = require('http');
+var config = require('./package.json');
+
+
+var server = http.createServer(function(req, res) {
+  // You can define here your custom logic to handle the request
+  // and then proxy the request.
+  console.log(req.domain);
+  console.log(req.headers.host);
+  res.write("hello from app1");
+  res.end();
+});
+
+server.listen(config.port);
